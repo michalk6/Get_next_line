0cedf51dd7c87c7ceb8a570fb71faddfd0a74b4e
Michal Kurek <michalkurek92@gmail.com>
2024-12-18 21:51:53 +0100

    project ready to test

--- get_next_line.c
+++ get_next_line.c
@@ -6,33 +6,64 @@
 /*   By: mikurek <mikurek@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 18:17:13 by mikurek           #+#    #+#             */
-/*   Updated: 2024/12/16 23:31:15 by mikurek          ###   ########.fr       */
+/*   Updated: 2024/12/18 21:49:19 by mikurek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 	#include <stdio.h>
 
-char	*ft_get_line(char **stream)
+int	ft_check_for_line(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_read_file(int fd, char *stream)
+{
+	char	*buffer;
+	int		bytes_read;
+
+	bytes_read = BUFFER_SIZE;
+	while (!ft_check_for_line(stream))
+	{
+		buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		buffer[BUFFER_SIZE] = 0;
+		stream = ft_strjoin(stream, buffer);
+		if (bytes_read < BUFFER_SIZE)
+			break ;
+	}
+	return (stream);
+}
+
+char	*ft_cut_stream(char *stream)
+{
+	int		i;
+
+	i = 0;
+	while (stream[i] && stream[i] != '\n')
+		i++;
+	return (ft_substr(stream, i + 1, ft_strlen(stream) - i));
+}
+
+char	*ft_read_line(char *stream)
 {
 	char	*line;
 	int		i;
 
 	i = 0;
-	while (*stream[i] != '\n')
-	{
-		printf("robię stream %d \n", i);
-		if (!(*stream[i])){
-		printf("zwracam null \n");
-			return (NULL);}
+	while (stream[i] && stream[i] != '\n')
 		i++;
-	}
-	line = malloc((i + 1) * sizeof(char));
-	if (!line)
-		return (NULL);
-	line = ft_substr(*stream, 0, i);
-		printf("in ft_get_line stream = %s", stream);
-	stream = &stream[i];
+	line = ft_substr(stream, 0, i + 1);
 	return (line);
 }
 
@@ -41,24 +72,19 @@ char	*get_next_line(int fd)
 	static char	*stream;
 	char		*buffer;
 	char		*line;
-		printf("declarations in get line\n");
+
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	if (!stream)
+	if (stream == NULL)
 		stream = "";
-		printf("stream = %s\n", stream);
-	line = 0;
-		printf("line = %s\n", line);
-
-	while (!line)
+	if (!ft_check_for_line(stream))
+		stream = ft_read_file(fd, stream);
+	line = ft_read_line(stream);
+	if (*stream == 0)
 	{
-			printf("in while\n");
-		line = ft_get_line(&stream);
-		buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
-		if (!buffer)
-			return (NULL);
-		read(fd, buffer, BUFFER_SIZE);
-		stream = ft_strjoin(stream, buffer);
+		free(stream);
+		return (NULL);
 	}
+	stream = ft_cut_stream(stream);
 	return (line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: mikurek <mikurek@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 18:31:45 by mikurek           #+#    #+#             */
-/*   Updated: 2024/12/16 22:50:52 by mikurek          ###   ########.fr       */
+/*   Updated: 2024/12/18 21:43:19 by mikurek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFFER_SIZE 20
+# define BUFFER_SIZE 4
 
 # include <unistd.h>
 # include <stdlib.h>
